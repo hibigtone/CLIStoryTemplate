ca10f448687e37a2246a3e21584d039b17752057
C.Holder <sessions@16bitsound.live>
2023-10-19 16:16:43 -0400

    add story line

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -8,5 +8,6 @@ import Foundation
 func chapterOne() {
     print("peace")
     print("whats good")
+    print("the storm rolled thru on a warm sunday evening")
     // Your portion of the story goes here
 }
