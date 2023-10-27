b9de897416fcfc146f18bfb4cf0d1c5f61ab3672
Antonio Gregory <antonio.dgregory@gmail.com>
2023-10-27 16:12:27 -0400

    Additions to chap 1

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -6,9 +6,10 @@
 import Foundation
 
 func chapterOne() {
-    print("peace")
-    print("whats good")
-    print("test")
+    let disease = "blank"
+    let sisterName = "unkown"
+    print("10 years. 10 year since dispair last ran rampant throughout the world. 10 years since \(disease) claimed the lifes of tens of millions. 10 years since worldwide terror quickly shifted to worldwide celebration. 10 years since the vaccine was invinted, 10 years since the world was saved, and 10 years since my world stopped. 10 years. But it's barely been 10 seconds since I stumbled across an old, tattered news article. The article about my sister, \(sisterName); \(disease)'s last fatality.")
+ 
     // Your portion of the story goes here
 }
 //
