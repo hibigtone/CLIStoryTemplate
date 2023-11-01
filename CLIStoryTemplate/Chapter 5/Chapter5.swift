95adeabb6e9302cc5ae66c961425a3ec2c76a171
Essence <cconeal95@gmail.com>
2023-11-01 15:51:08 -0400

    chapter 5 & 1

--- CLIStoryTemplate/Chapter 5/Chapter5.swift
+++ CLIStoryTemplate/Chapter 5/Chapter5.swift
@@ -6,7 +6,27 @@
 import Foundation
 
 func chapterFive() {
-let personOne:String = "Kassima"
+    let personOne:String = "Kassima"
+    let personTwo:String = "Azura"
+    let personThree:String = "Jabari"
+
+
+        print("\(personOne) took in her surroundings, and let out a sigh of relief once she realized she'd returned to the present.")
+        print("However, her contentment was short lived. Panic seeped in; she'd wasted her last opportunity to save her sister.")
+        print("\(personOne), in a frantic state, called out for \(personThree). '\(personThree), where are you??!'")
+
+    //    if numberOfTravelTimes >= 1 {
+    //        print("After many attempts, /(personTwo) Kassima is finally able to return to the year of 1990.")
+    //
+    //    }
+    //
+    //    else {
+    //        print("Kassima further struggles to jump back in time, 'Why isn't this working!' She shouted. before inevitably accepting that she can")
+    //    }
+    //
+
+    }
+
 let personThree:String = "Jabari"
 
 
