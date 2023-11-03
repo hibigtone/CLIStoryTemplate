53112ddef334c032e08a949348472384d0efc482
Antonio Gregory <antonio.dgregory@gmail.com>
2023-11-03 15:05:54 -0400

    chap 1 made functional

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -5,29 +5,42 @@
 
 import Foundation
 
+
+
+
+
 func chapterOne() {
     let disease: String = "blank"
-    let sisterName: String = "unkown"
+    let mainCharacter: String = "Kassima"
+    let sisterName: String = "Azura"
     let sisterDeath: Bool = true
+    let demonName: String = ""
     let timePassed: Int = 10
-   
-    func paragrahOne() {
-        print("Would you like to read chapter one Yes or No")
+    
+    
+    func chapterBegin() {
+        let paragraphOne: String = "\(timePassed) years. \(timePassed) year since dispair last ran rampant throughout the world. \(timePassed) years since \(disease) claimed the lifes countless individuals. \(timePassed) years since an era of worldwide terror shifted to an era of worldwide celebration. \(timePassed) years since the vaccine was invinted, \(timePassed) years since the world was saved, and \(timePassed) years since my world stopped. \(timePassed) years. But it's barely been \(timePassed) seconds since I stumbled across an old, tattered newspaper. The  article about my sister, \(sisterName); \(disease)'s last fatality. \(sisterDeath), \(mainCharacter), \(demonName)"
+        print("Would you like to read chapter one? Yes or No.")
         
         if let response = readLine() {
             if response == "Yes" {
-                paragrahOne()
+                print("\(paragraphOne)")
             }
             
-            if response == "No" {
+            else if response == "No" {
                 chapterTwo()
             }
-                    print("\(timePassed) years. \(timePassed) year since dispair last ran rampant throughout the world. \(timePassed) years since \(disease) claimed the lifes of tens of millions. \(timePassed) years since worldwide terror quickly shifted to worldwide celebration. \(timePassed) years since the vaccine was invinted, \(timePassed) years since the world was saved, and 10 years since my world stopped. 10 years. But it's barely been 10 seconds since I stumbled across an old, tattered news article. The article about my sister, \(sisterName); \(disease)'s last fatality. \(sisterDeath)")
+            else {
+                print("Error: Invalid responce");chapterBegin()
+            }
+                                
+            }
     }
-    
- 
-     
+    func chapterMiddle() {
     }
+
+
     // Your portion of the story goes here
+chapterBegin()
 }
 //
