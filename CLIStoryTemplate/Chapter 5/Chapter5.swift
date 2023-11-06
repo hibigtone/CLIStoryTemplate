0434eed004e2e02f8af10bf6a1154e1f58f7938a
Essence <cconeal95@gmail.com>
2023-11-06 14:58:19 -0500

    chap 5

--- CLIStoryTemplate/Chapter 5/Chapter5.swift
+++ CLIStoryTemplate/Chapter 5/Chapter5.swift
@@ -18,9 +18,12 @@ func chapterFive() {
     print("\(personOne) took in her surroundings, and let out a sigh of relief once she realized she'd returned to the present.")
     print("However, her contentment was short lived. Panic seeped in; she'd wasted her last opportunity to save her sister.")
     print("\(personOne), in a frantic state, called out for \(personThree). '\(personThree), where are you??!'")
-    print("'I know you're there; you have to send me back!'")
-    print("")
-
+    print("'I know you're there; you have to send me back!' Kassima plead.")
+    print("When she realized she wasn't getting a response she sat down on the bed behind her, and palmed her face with a sigh. Feeling totally helpless, she began to weep.")
+    print("'Quite the show.' \(personOne) heard a voice to her left, causing her to veer in that direction where \(personThree) lounged on her bed.")
+    print("'You!' \(personOne) pointed accusingly. She wiped the tears from her eyes, 'You have to send me back-!'")
+    print("\(personThree) raised their hand to silenece Kassima. 'Wrong.'")
+    
     //    if numberOfTravelTimesRemaining >= 1 {
     //        print("After many attempts, /(personTwo) Kassima is finally able to return to the year of 1990.")
     //
