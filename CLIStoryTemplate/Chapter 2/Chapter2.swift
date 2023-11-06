adc5988b9beedab5fafce71c734a6f5ca7155871
C.Holder <sessions@16bitsound.live>
2023-11-06 14:26:41 -0500

    push

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -4,11 +4,13 @@
 //
 
 import Foundation
+let sisterone:String = "Ohana"
+let sistertwo:String = "Millie"
+let momma:String = "Angie"
+let poppa:String = "Roe"
+let brotherone:String = "lilRoe"
+var numberOfsnakes:Int = 3
+//
 
-func chapterTwo() {
-    print("Tone")
-    // Your portion of the story goes here
-
-    
-}
+print("Every day we counted down the days til momma and daddy would arrive.Day after day brother would arise before the sun and head down to the city center to catch the latest news and dos. Then one day…a strange frail man who radiated a very creepy sorcerer type energy arrived at daddy’s house with a odd envelope. Press 1 to open Press 2 to discard envelope. Because of that strange visit i've  been having strange dreams where voices and shadows visit. At times I awaken at akward hours of the night its then when im faced with the decision of staying in bed or faces my fears of my daddy old crinkly house. Press 3 to stay in bed Press 4 to walk down hall to investigate weird sound")
 
