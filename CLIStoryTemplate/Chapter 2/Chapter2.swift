2d395b0412c97d8789c0dcbc758f1408d4cae7c5
C.Holder <sessions@16bitsound.live>
2023-11-07 15:52:44 -0500

    done

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -11,12 +11,46 @@ func chapterTwo() {
     let momma:String = "Angie"
     let poppa:String = "Roe"
     let brother:String = "lilRoe"
-    let timepassed:Int = 8
-    var creppynoise:[String] = ["catcry", "owlhoot", "windblowing"]
-    if let creppy = creppynoise.randomElement(){
-        print("The noise we chose is \(creppy)")
+    let count = [1,2,3,4,5,6,7,8]
+   
+    func wakeUP() {
+        struct Person {
+            var clothes:String
+            var shoes:String
+            var kassima: Bool
+            
+            
+        }
+        let Kassima = Person(clothes: "gown", shoes: "slippers", kassima: true)
     }
     
-    print("Everyday \(personOne) and \(personTwo) counted down the days til \(momma) and \(poppa)daddy would arrive.\(timepassed)Day after day \(brother) would arise before the sun and head down to the city center to catch the latest news and dos. Then one day…a strange frail man who radiated a very creepy sorcerer type energy arrived at \(poppa) house with a odd envelope. Press 1 to open Press 2 to discard envelope. Because of that strange visit i've  been having strange dreams where voices and shadows visit. At times \(personOne) awaken at akward hours of the night its then when im faced with the decision of staying in bed or faces my fears of my \(poppa) old crinkly house. Press 3 to stay in bed Press 4 to walk down hall to investigate weird sound")
-    
-}
+    let timepassed:Int = 8
+    let creppynoise:[String] = ["catcry", "owlhoot", "windblowing"]
+    if let creppy = creppynoise.randomElement(){
+        print("The noise we chose is \(creppy)")
+        
+    }
+    func Chapter2(){ print("Would you like to read chapter two? Yes or No.")
+        
+        if let response = readLine() {
+            if response == "Yes" {
+                print("Everyday \(personOne) and \(personTwo) counted down the days til \(momma) and \(poppa) would arrive,\(timepassed) Days went by. Each morning \(brother) would arise before the sun and head down to the city center to catch the latest news and dos. While \(brother) was downtown a strange frail man who radiated a strang energy arrived at \(poppa) house with an odd envelope. Because of that strange visit i've  been having dreams where voices and shadows visit. At times \(personOne) awaken at akward hours of the night its then when im faced with the decision of staying in bed or faces my fears of my \(poppa) old crinkly house. \(personOne) walked down hall to investigate weird sound.")
+                for deepbreaths in count {print("deepbreath \(deepbreaths)")
+                }
+                print("When I mustared up the coruage to see what was up i was startled by \(brother) However \(brother) ")
+            }
+            
+            else if response == "No" {
+                chapterThree()
+            }
+            else {
+                print("Error: Invalid responce");Chapter2()
+            }
+                                
+            }
+        
+    }
+    Chapter2()
+        
+    }
+
